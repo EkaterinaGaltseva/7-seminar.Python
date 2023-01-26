6db70ea796af1585c7ed1da2888ac93fc8e38c59
Екатерина Гальцева <katerina.vedernikova95@mail.ru>
2023-01-26 14:25:30 +0500

    Создали файл readme с описанием нужной работы

---
+++ dobavl_pol.py
@@ -0,0 +1,25 @@
+def add_surname(d):
+    with open('telefon_book', 'a') as file:
+        file.write('фамилия: {}\n'
+        .format(d))
+
+def add_name(d):
+    with open('telefon_book', 'a') as file:
+        file.write('имя: {}\n'
+        .format(d))
+
+def add_number(d):
+    from tabulate import tabulate1
+    print = ["Фамилия", "Имя", "Номер телефона", "Опичание"]
+
+def add_data(d):
+    with open('telefon_book', 'a') as file:
+        file.write('номер телефона: {}\n'
+        .format(d))
+
+def add_description(d):
+
+        file.write(tabulate(d, headers ='head', tablefmt="grid"))
+
+with open ('telefon_book.bd', 'a') as file:
+        file.write('Описание: {}\n'.format (d))
---
+++ telef.py
@@ -0,0 +1,10 @@
+import re
+from tokenize import Number
+
+def get_surname():
+    surname = input('Фамилия: ')
+    return surname
+
+def get_data():
+    surname = input('Фамилия: ')
+    
